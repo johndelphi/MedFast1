263f0d1371a4920e06c662faf7a4877ea7093c38
John.michael <John.michael@studentambassadors.com>
2022-01-19 11:33:05 -0600

    Add project files.

---
+++ Data/ShopContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedFastreact.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options): base(options)
+            {
+}
+        public DbSet<Shops> Shop { get; set; } = null!;
+//protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+            //optionsBuilder.UseSqlServer("server =.; Database=MEDfastAPI; Intergrated Security=True");
+            //base.OnConfiguring(optionsBuilder);
+        //}
+
+    }
+}
