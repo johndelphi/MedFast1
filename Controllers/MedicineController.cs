9dbea465ba6e2942a3995cd228f790610006fc82
John.michael <John.michael@studentambassadors.com>
2022-01-31 08:15:24 -0600

    added drug controller a

---
+++ Controllers/MedicineController.cs
@@ -0,0 +1,20 @@
+﻿using MedFast1.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedFast1.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class MedicineController : ControllerBase
+{
+  private readonly IMedicineRepository _medecineRepository;
+  public MedicineController(IMedicineRepository medicinerepository)
+  {
+    _medecineRepository = medicinerepository;
+  }
+  [HttpGet]
+    public async Task<IActionResult> GetAllDrugs()
+  {
+    var drugs = await _medecineRepository.GetAllDrugsAsync();
+    return Ok(drugs);
+  }
+}
